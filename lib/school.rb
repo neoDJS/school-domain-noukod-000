dc453b657a767e18492d9d6c3ea32333be0b175c
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-03 16:57:24 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
 # code here!
+
+class School
+  attr_reader :name, :roster
+  def initiaize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = [] if @roster[grade].nil?
+    @roster[grade] << name
+  end
+end
