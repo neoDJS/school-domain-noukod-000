9d08698d206280ecdf6613d6e5db8cbed6561702
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-03 17:37:04 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+
+class School
+  attr_reader :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = [] if @roster[grade].nil?
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.keys.each{|k| @roster[k].sort!}
+    @roster
+  end
+end
